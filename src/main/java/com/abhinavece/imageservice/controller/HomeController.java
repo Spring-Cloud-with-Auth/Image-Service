e62d806793b031d282d04a610b14de6ebcda2ae0
Your Name <you@example.com>
2018-07-10 00:10:19 +0530

    first commit

---
+++ src/main/java/com/abhinavece/imageservice/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.abhinavece.imageservice.controller;
+
+import com.abhinavece.imageservice.Model.Image;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/images")
+    private List<Image> getImages(){
+        List<Image> images = Arrays.asList(
+                new Image(1, "Treehouse of Horror V", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3842005760"),
+                new Image(2, "The Town", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3698134272"),
+                new Image(3, "The Last Traction Hero", "https://www.imdb.com/title/tt0096697/mediaviewer/rm1445594112"));
+        return images;
+    }
+
+    @RequestMapping("/{id}")
+    public Image getImageById(@PathVariable("id") int id){
+        return  new Image(id, "Just Hardcoded Value", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3842005760");
+    }
+}
